fa7f193280be31d77c11781559315b4af89702a6
Shinichiro Honda <shinichiro13579@gmail.com>
2025-06-04 12:37:23 +0900

    Initial Commit

---
+++ Kagene_Studio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kagene_Studio
+//
+//  Created by 本多真一朗 on 2025/06/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kagene_Studio/Kagene_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kagene_StudioApp.swift
+//  Kagene_Studio
+//
+//  Created by 本多真一朗 on 2025/06/04.
+//
+
+import SwiftUI
+
+@main
+struct Kagene_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
