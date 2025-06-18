c2de796cc0989f24d3c239afbf307b42c71a833f
Shinichiro Honda <shinichiro13579@gmail.com>
2025-06-18 19:13:04 +0900

    ボタンにアニメーションをつけた。

--- Voisync/OnGoing/RecordingView.swift
+++ Voisync/OnGoing/RecordingView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct RecordingView: View {
     @StateObject private var recordingVM = RecordingViewModel()
     @StateObject private var recordPlayingVM = RecordPlayingViewModel()
+    @State private var isPressing = false
+
     
     var body: some View {
         VStack(spacing: 24) {
@@ -19,9 +21,20 @@ struct RecordingView: View {
                 .font(.system(size: 24, weight: .bold))
                 .padding(.horizontal, 24)
                 .padding(.vertical, 12)
-                .background(Color.red)
+                .background(isPressing ? Color.red.opacity(0.7) : Color.red)
                 .foregroundColor(.white)
                 .clipShape(Capsule())
+                .scaleEffect(isPressing ? 0.9 : 1.0)
+                .animation(.easeInOut(duration: 0.2), value: isPressing)
+                .onLongPressGesture(minimumDuration: 0.1, pressing: { pressing in
+                    isPressing = pressing
+                    if pressing {
+                        recordingVM.startRecording()
+                    } else {
+                        recordingVM.stopRecording()
+                        recordPlayingVM.loadAudio()
+                    }
+                }, perform: {})
 //            Button("Record") {
 //                recordPlayingVM.loadAudio()
 //                recordingVM.startRecording()
@@ -39,8 +52,4 @@ struct RecordingView: View {
             }
         }
     }
-}
-
-#Preview {
-    RecordingView()
 }
